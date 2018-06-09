dd96e35d70c6605bbf334bcee939f9bd2df0cca1
bosak <bosakmaw@gmail.com>
2018-06-09 23:22:15 +0300

    create empty vue app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    font-family: Fontin, sans-serif;
+    box-sizing: border-box;
+}
+
+body {
+    background-image: url('https://web.poecdn.com/gen/image/YTozOntpOjA7aTowO2k6/MjthOjI6e3M6MjoiaWQi/O2k6OTE3O3M6NDoic2l6/ZSI7czo0OiJvcmlnIjt9/aToxO2k6NDt9/8d591da8f8/Act5.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    color:white;
+}
+</style>
